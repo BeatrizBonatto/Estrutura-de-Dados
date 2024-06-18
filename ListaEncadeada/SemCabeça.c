9b8c12e05c5261bb6012fd54e6884dfc66e4a5bd
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2024-06-17 22:50:36 -0300

    feat: add fila

--- ListaEncadeada/SemCabeça.c
+++ ListaEncadeada/SemCabeça.c
@@ -1,56 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+struct node {
   int numero;
   struct node *prox;
 };
 typedef struct node no;
 
-no *cria_lista(int numero){
+no *cria_lista(int numero) {
   no *n = malloc(sizeof(struct node));
   n->numero = numero;
   n->prox = NULL;
   return n;
-};
+}
 
 void adicionar(no **inicio, no **fim, int numero) {
   no *n = cria_lista(numero);
-  if (*inicio == NULL && *fim == NULL){
+  if (*inicio == NULL && *fim == NULL) {
     *inicio = n;
     *fim = n;
-  }
-  else {
+  } else {
     (*fim)->prox = n;
-    (*fim) = n; // Atualiza o ponteiro para o último elemento
+    *fim = n; // Atualiza o ponteiro para o último elemento
   }
 }
 
-int excluir(no** inicio, no** fim) {
-    if (*inicio == NULL) {
-        // Lista vazia
-        printf("\nLista esta vazia\n");
-        return;
-    }
-    if (*inicio == *fim) {
-        // Lista tem apenas um elemento
-        free(*inicio);
-        *inicio = *fim = NULL;
-        return;
-    }
-    // Remove o primeiro nó
-    no* temp = *inicio;
-    temp = temp->prox;
-    free(*inicio);
-    *inicio = temp;
+int excluir(no **inicio, no **fim) {
+  if (*inicio == NULL) {
+    // Lista vazia
+    printf("\nTodos os valores ja foram somados, sua lista esta vazia\n");
+    return 0;
+  }
 
-    printf("\nResultado\nI -> %d F -> %d\n", (*inicio)->numero, (*fim)->numero);
-    return (*inicio)->numero;
+  int valor_excluido = (*inicio)->numero;
+
+  if (*inicio == *fim) {
+    // Lista tem apenas um elemento
+    free(*inicio);
+    *inicio = *fim = NULL;
+  } else {
+    // Remove o primeiro nó
+    no *temp = *inicio;
+    *inicio = (*inicio)->prox;
+    free(temp);
+  }
+
+  return valor_excluido;
 }
 
 void imprime(no **inicio, no **fim) {
   no *i;
-  for (i=*inicio; i!=NULL; i=i->prox){
+  for (i = *inicio; i != NULL; i = i->prox) {
     printf("%d\n", i->numero);
   }
 }
@@ -61,33 +61,21 @@ int main() {
   inicio = NULL;
   fim = NULL;
 
+  int soma = 0;
+
   adicionar(&inicio, &fim, 5);
   adicionar(&inicio, &fim, 7);
   adicionar(&inicio, &fim, 15);
 
-  //imprimir lista com todos os itens
-  imprime (&inicio, &fim);
+  // Imprimir lista com todos os itens
+  imprime(&inicio, &fim);
 
-  //print meu inicio e fim salvos
-  printf("\nValores iniciais\nI -> %d F -> %d\n", inicio->numero, fim->numero);
+  soma = soma + excluir(&inicio, &fim);
+  soma = soma + excluir(&inicio, &fim);
+  soma = soma + excluir(&inicio, &fim);
+  soma = soma + excluir(&inicio, &fim);
 
-  //excluir 15 da lista
-  excluir(&inicio, &fim);
+  printf("Total soma: %d\n", soma);
 
-  imprime (&inicio, &fim);
-
-  //excluir 7 da lista
-  excluir(&inicio, &fim);
-
-  imprime (&inicio, &fim);
-
-  //excluir 5 da lista
-  excluir(&inicio, &fim);
-
-  imprime (&inicio, &fim);
-
-  //tenta excluir, mas lista esta vazia
-  excluir(&inicio, &fim);
-  //imprimir mensagem de lista vazia
-  imprime (&inicio, &fim);
+  return 0;
 }
